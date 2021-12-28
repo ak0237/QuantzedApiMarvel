77a0f0458a90f8f4e432386da6066a53af9fa863
Andrey <andrey.karvat123@hotmail.com>
2021-12-28 02:56:40 -0300

    Função random aprimorada

--- script.js
+++ script.js
@@ -5,25 +5,34 @@ document.head.appendChild(scriptTag)*/
 function fetchApiMarvel() {
     
     
-
+    
     const ts = '1640660504620'
     const apiKey = '83b88d0adfd986c16eabc702bf00a366'
     const hash = 'e64b1dfab1dd4630fb069d80f1f85755'
 
-    fetch(`http://gateway.marvel.com/v1/public/characters?ts=${ts}&apikey=${apiKey}&hash=${hash}&limit=50`)
+    fetch(`http://gateway.marvel.com/v1/public/characters?ts=${ts}&apikey=${apiKey}&hash=${hash}`)
     .then((response) => {
 
         return response.json()})
     .then((objeto) => {
         console.log(objeto)
-        const keys = Object.keys(objeto.data.results)
-        const randIndex = Math.floor(Math.random() * keys.length)
-        const randKey = keys[randIndex]
-        const randomValue = objeto.data.results[randKey]
-        console.log(randomValue)
 
+        //Código para definir quantos heróis estarão ativos 
+        var seed = Math.floor(Math.random() * 9) + 1 
+        var ativos = []
+
+        //Loop para aleatorizar os heróis ativos
+        for(let indice = 0; indice < seed; indice++){
+             ativos [indice] = randomizer(objeto)
+        }
+        
+        console.log(ativos)
+
+
+        //Encontra onde os heróis aparecerão na página HTML
         const herois = document.querySelector('#herois_ativos')
         
+        //Renderiza os heróis no HTML
         objeto.data.results.forEach((item) => {
         
             const nomeHeroi = item.name
@@ -33,6 +42,15 @@ function fetchApiMarvel() {
             
         })
     })
+}
+
+function randomizer(objeto){
+    const keys = Object.keys(objeto.data.results)
+    const randIndex = Math.floor(Math.random() * keys.length)
+    const randKey = keys[randIndex]
+    const randomValue = objeto.data.results[randKey]
+    //console.log(randomValue)
+    return randomValue
 }
 
 function criarDiv(srcImagem, nomeHeroi, divHeroi){
